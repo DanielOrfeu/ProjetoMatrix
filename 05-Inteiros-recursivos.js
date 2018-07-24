d5d471f344755a267b583931287f4e075641940f
Daniel Yago Orfeu <daniel_yago1997@hotmail.com>
2018-07-24 19:01:57 -0300

    Comentário com esclarecimentos de uma função.
    Comentário com esclarecimentos do funcionamento da função "multiplicarInteiros(n)" inserido.

--- 05-Inteiros-recursivos.js
+++ 05-Inteiros-recursivos.js
@@ -30,7 +30,7 @@ function somarImpares(n){
   }
 }
 
-function multiplicarInteiros(n){
+function multiplicarInteiros(n){ //Esta função não se baseia em fatorial de "n" (n!), tendo em mente que uma entrada de n===0 neste caso retorna 0, não 1.
   function operarMultiplicacao(n){
     return n===1? 1 : n*operarMultiplicacao(n-1);
   }
