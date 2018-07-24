0158362f7d5407d0a62e9d4ce9f8b2abf9133a12
Daniel Yago Orfeu <daniel_yago1997@hotmail.com>
2018-07-24 16:45:36 -0300

    Inserção de verificador de número negativo

--- 01-Soma_de_inteiros.js
+++ 01-Soma_de_inteiros.js
@@ -1,7 +1,7 @@
 //Daniel Yago da Silva Orfeu Joaquim
 
 function somarInteiros(n){
-  if(typeof n==="number" && n===Math.trunc(n)){
+  if(typeof n==="number" && n===Math.trunc(n) && n>=0){
     var res=0;
     for(var i=0; i<=n; i++){
       res+=i;
